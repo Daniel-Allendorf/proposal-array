c046677be97ce6bb5192cbacca763b8c5dac531b
D. Allendorf <92928555+Daniel-Allendorf@users.noreply.github.com>
2023-02-11 10:23:52 +0100

    Optimize Alias Table construction.

--- include/sampling/AliasTable.hpp
+++ include/sampling/AliasTable.hpp
@@ -9,60 +9,43 @@ namespace sampling {
 class AliasTable {
 public:
     AliasTable(const std::vector<double>& weights) :
-        table_(weights.size()), entry_dist_(0, weights.size() - 1), real_dist_(0, 1) {
+            table_(weights.size()), entry_dist_(0, weights.size() - 1), real_dist_(0, 1) {
         assert(weights.size() > 0);
         size_t N = weights.size();
         double W = 0;
         for (auto w : weights) W += w;
-        std::vector<size_t> overfull;
-        std::vector<size_t> underfull;
-        overfull.reserve(N);
-        underfull.reserve(N);
+        std::vector<size_t> hi(N); size_t hi_size = 0;
+        std::vector<size_t> lo(N); size_t lo_size = 0;
         for (size_t i = 0; i < N; ++i) {
-            double p_i = weights[i] / W;
-            size_t element = i;
-            size_t alias = N;
-            double threshold = N * p_i;
-            if (threshold < 1.0) {
-                underfull.push_back(i);
-            } else if (threshold > 1.0) {
-                overfull.push_back(i);
-            } else {
-                threshold = 1.0;
-                alias = i;
+            double t = N * (weights[i] / W);
+            if (t < 1.0) {
+                lo[lo_size] = i; lo_size++;
+            } else if (t > 1.0) {
+                hi[hi_size] = i; hi_size++;
             }
-            table_[i] = std::make_tuple(element, alias, threshold);
+            table_[i] = std::make_tuple(i, i, t);
         }
-        while (!underfull.empty() || !overfull.empty()) {
-            if (overfull.empty()) {
-                size_t i = underfull.back();
-                underfull.pop_back();
-                table_[i] = std::make_tuple(i, i, 1.0);
-                continue;
+        while (lo_size > 0 && hi_size > 0) {
+            size_t i = lo[lo_size - 1]; lo_size--;
+            size_t j = hi[hi_size - 1]; hi_size--;
+            double t_i = std::get<2>(table_[i]);
+            double t_j = std::get<2>(table_[j]);
+            t_j += t_i - 1.0;
+            if (t_j < 1.0) {
+                lo[lo_size] = j; lo_size++;
+            } else if (t_j > 1.0) {
+                hi[hi_size] = j; hi_size++;
             }
-            if (underfull.empty()) {
-                size_t j = overfull.back();
-                overfull.pop_back();
-                table_[j] = std::make_tuple(j, j, 1.0);
-                continue;
-            }
-            size_t i = underfull.back();
-            size_t j = overfull.back();
-            underfull.pop_back();
-            overfull.pop_back();
-            auto [element_i, alias_i, threshold_i] = table_[i];
-            auto [element_j, alias_j, threshold_j] = table_[j];
-            double threshold = threshold_i + threshold_j - 1.0;
-            if (threshold < 1.0) {
-                underfull.push_back(j);
-            } else if (threshold > 1.0) {
-                overfull.push_back(j);
-            } else {
-                threshold = 1.0;
-                alias_j = j;
-            }
-            table_[i] = std::make_tuple(element_i, j, threshold_i);
-            table_[j] = std::make_tuple(element_j, alias_j, threshold);
+            table_[i] = std::make_tuple(i, j, t_i);
+            table_[j] = std::make_tuple(j, j, t_j);
+        }
+        while (lo_size > 0) {
+            size_t i = lo[lo_size - 1]; lo_size--;
+            table_[i] = std::make_tuple(i, i, 1.0);
+        }
+        while (hi_size > 0) {
+            size_t j = hi[hi_size - 1]; hi_size--;
+            table_[j] = std::make_tuple(j, j, 1.0);
         }
     }
 
